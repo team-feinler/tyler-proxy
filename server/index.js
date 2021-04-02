3be48ea6e8b58a0f5aba575b6b2e4e7787b85f5a
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-04-01 22:10:22 -0700

    Refactor proxy to forward route for service

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const axios = require('axios');
 const port = 3000;
 const app = express();
 
@@ -12,4 +13,17 @@ app.use(cors());
 app.use(express.static(`${__dirname}/../public`));
 app.use('/:id', express.static(`${__dirname}/../public`));
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.get('/product-features/:id', async (req, res) => {
+  const { params: { id } } = req;
+
+  try {
+    const { data } = await axios.get(`http://localhost:4000/product-features/${id}`);
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
