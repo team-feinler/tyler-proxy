dec468ebc062cd7dd4235f607120db143485f61b
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-04-08 17:39:50 -0700

    Confirm artillery tests run successfully

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 // app.use(morgan('dev'));
 
 app.use(cors());
+app.use(express.json());
 app.use(express.static(`${__dirname}/../public`));
 app.use('/:id', express.static(`${__dirname}/../public`));
 
@@ -22,9 +23,10 @@ app.post('/product-features', async (req, res) => {
   };
 
   try {
-    const response = await axios.post('http://localhost:4000/product-features', postData);
-    res.send(response);
+    const { data } = await axios.post('http://localhost:4000/product-features', postData);
+    res.send(data);
   } catch(err) {
+    console.log('ERROR', err);
     res.status(500).send(err);
   }
 });
@@ -34,14 +36,9 @@ app.get('/product-features/:id', async (req, res) => {
 
   try {
     const { data } = await axios.get(`http://localhost:4000/product-features/${id}`);
-    res.send(data);
   } catch (err) {
     res.status(500).send(err);
   }
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
 });
 
 app.put('/product-features/:id', async (req, res) => {
@@ -71,4 +68,8 @@ app.delete('/product-features/:id', async (req, res) => {
   } catch(err) {
     res.status(500).send(err);
   }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
