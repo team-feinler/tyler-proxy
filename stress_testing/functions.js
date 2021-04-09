1fcf2eb67c7b593a84f86b3bbaf5fcd8b1929042
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-04-08 17:24:26 -0700

    Add/configure artillery

---
+++ stress_testing/functions.js
@@ -0,0 +1,25 @@
+const { generatefFeaturesTableRecord } = require('./data_generator.js');
+
+function setUrlWithProductId(requestParams, context, ee, next) {
+  const bottomTenPercentId = 9000000 + Math.ceil(Math.random() * 1000000);
+  const url = `/product-features/${bottomTenPercentId}`;
+
+  requestParams.url = url;
+  return next();
+}
+
+function setFakeDataRecord(requestParams, context, ee, next) {
+  const json = {
+    table: 'features',
+    record: generatefFeaturesTableRecord(),
+  };
+
+  requestParams.json = json;
+  return next();
+}
+
+
+module.exports = {
+  setUrlWithProductId,
+  setFakeDataRecord,
+}
