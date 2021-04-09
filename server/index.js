08b276dbb724cdae3fd26a247949f85f22673505
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-04-08 17:09:53 -0700

    Add CUD API routes

--- server/index.js
+++ server/index.js
@@ -13,6 +13,21 @@ app.use(cors());
 app.use(express.static(`${__dirname}/../public`));
 app.use('/:id', express.static(`${__dirname}/../public`));
 
+app.post('/product-features', async (req, res) => {
+  const { body: { table, record } } = req;
+  const postData = {
+    table: table,
+    record: record,
+  };
+
+  try {
+    const response = await axios.post('http://localhost:4000/product-features', postData);
+    res.send(response);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+});
+
 app.get('/product-features/:id', async (req, res) => {
   const { params: { id } } = req;
 
@@ -26,4 +41,33 @@ app.get('/product-features/:id', async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.put('/product-features/:id', async (req, res) => {
+  const { params: { id }, body: { table, updates } } = req;
+  const updateData = {
+    table: table,
+    updates: updates,
+  };
+
+  try {
+    const response = await axios.put(`http://localhost:4000/product-features/${id}`, updateData);
+    res.send(response);
+  } catch(err) {
+    res.status(500).send(err);
+  }
+});
+
+app.delete('/product-features/:id', async (req, res) => {
+  const { params: { id }, body: { table } } = req;
+  const deleteData = {
+    table: table,
+  };
+
+  try {
+    const response = await axios.delete(`http://localhost:4000/product-features/${id}`, deleteData);
+    res.send(response);
+  } catch(err) {
+    res.status(500).send(err);
+  }
 });
